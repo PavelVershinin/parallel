063f8b6dac0a0e8b4cd915307c8de4d647699d35
Вершинин Павел Николаевич <pavel@impaka.ru>
2021-11-21 06:51:34 +0300

    Возвращение слайса с ошибками

--- parallel.go
+++ parallel.go
@@ -1,34 +1,37 @@
 package parallel
 
 import (
+	"context"
 	"sync"
 )
 
 // Parallel Паралельно выполнит все функции fns
 // Время работы функции Parallel примерно равно времени работы самой долгой из переданных в Parallel фунций
-// Для досрочного выхода, закрыть канал cancel
-func Parallel(cancel <-chan struct{}, fns ...func()) {
+// По завершении вернёт слайс ошибок
+func Parallel(ctx context.Context, fns ...func() error) []error {
+	errors := make([]error, len(fns))
 	doneCh := make(chan struct{})
 
 	go func() {
 		wg := sync.WaitGroup{}
 		wg.Add(len(fns))
-		for _, fn := range fns {
-			go func(fn func()) {
+		for i, fn := range fns {
+			go func(fNum int, fn func() error) {
 				if fn != nil {
-					fn()
+					errors[fNum] = fn()
 				}
 				wg.Done()
-			}(fn)
+			}(i, fn)
 		}
 		wg.Wait()
 		doneCh <- struct{}{}
 	}()
 
 	go func() {
-		<-cancel
+		<-ctx.Done()
 		doneCh <- struct{}{}
 	}()
 
 	<-doneCh
+	return errors
 }
--- parallel_test.go
+++ parallel_test.go
@@ -1,6 +1,7 @@
 package parallel_test
 
 import (
+	"context"
 	"math"
 	"math/rand"
 	"sync/atomic"
@@ -11,14 +12,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func makeTestFuncs(count int, res *int32) (fns []func(), sumSleep, maxSleep time.Duration) {
+func makeTestFuncs(count int, res *int32) (fns []func() error, sumSleep, maxSleep time.Duration) {
 	for i := 0; i < count; i++ {
 		taskSleep := time.Millisecond * time.Duration(rand.Intn(100))
 		maxSleep = time.Duration(math.Max(float64(maxSleep.Milliseconds()), float64(taskSleep.Milliseconds()))) * time.Millisecond
 		sumSleep += taskSleep
-		fns = append(fns, func() {
+		fns = append(fns, func() error {
 			<-time.After(taskSleep)
 			atomic.AddInt32(res, 1)
+			return  nil
 		})
 	}
 
@@ -31,8 +33,10 @@ func TestParallel(t *testing.T) {
 
 	t.Run("the parallel function must be completed before the sum of the durations of all functions from the array and all functions from the array must have time to be executed", func(t *testing.T) {
 		res = 0
+
+		ctx := context.Background()
 		start := time.Now()
-		parallel.Parallel(nil, fns...)
+		parallel.Parallel(ctx, fns...)
 		elapsed := time.Since(start)
 
 		require.Less(t, elapsed, sumSleep)
@@ -41,19 +45,18 @@ func TestParallel(t *testing.T) {
 
 	t.Run("the parallel function must be completed earlier than the longest function from the array and not all functions from the array should have time to be executed", func(t *testing.T) {
 		res = 0
-		cancelAfter := maxSleep / 2
-		cancelCh := make(chan struct{})
-
-		go func() {
-			<-time.After(cancelAfter)
-			cancelCh <- struct{}{}
-		}()
+		ctx, _ := context.WithDeadline(context.Background(), time.Now().Add(maxSleep / 2))
 
 		start := time.Now()
-		parallel.Parallel(cancelCh, fns...)
+		parallel.Parallel(ctx, fns...)
 		elapsed := time.Since(start)
 
 		require.Less(t, elapsed, maxSleep)
 		require.Less(t, atomic.LoadInt32(&res), int32(15))
+	})
+
+	t.Run("the number of errors returned must be equal to the number of functions passed", func(t *testing.T) {
+		errs := parallel.Parallel(context.Background(), fns...)
+		require.Equal(t, len(fns), len(errs))
 	})
 }
